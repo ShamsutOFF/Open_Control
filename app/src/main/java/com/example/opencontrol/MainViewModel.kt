2cd891fffd8f1b19dd2ee50ced117305d5be855f
ag.shevchenko <ag.shevchenko@tinkoff.ru>
2023-06-09 12:44:58 +0300

    Изменила UI чата & попробовала записывать креды в shared_prefs

--- app/src/main/java/com/example/opencontrol/MainViewModel.kt
+++ app/src/main/java/com/example/opencontrol/MainViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.opencontrol
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.opencontrol.domain.MainRepository
 import com.example.opencontrol.model.ChatMessage
@@ -31,9 +35,18 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 class MainViewModel(
+    context: Application,
     private val repository: MainRepository,
     private val knoDao: KnoDao
-) : ViewModel() {
+) : AndroidViewModel(context) {
+    companion object {
+        val USERID_TAG = "user_id"
+        val START_MESSAGE = ChatMessage(
+            "Здравствуйте! Я бот-помощник. Чем могу помочь?",
+            false
+        )
+    }
+
     var userId by mutableStateOf("")
     var userInfo by mutableStateOf(UserInfoNetwork(userId, "", "", "", "", 0L, 0L))
         private set
@@ -49,9 +62,13 @@ class MainViewModel(
     var freeWindows = mutableStateListOf<FreeWindow>()
     var businessAppointments = mutableStateListOf<Appointments>()
 
+    //save creds
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+    private val sharedPreferencesEditor: SharedPreferences.Editor = sharedPreferences.edit()
+
     init {
         Timber.d("@@@ init")
-        chatListOfMessages.add(ChatMessage("Здравствуйте! Я бот-помощник. Чем могу помочь?", false))
+        chatListOfMessages.add(START_MESSAGE)
         downloadKnosToDatabase()
         getKnosFromRoom()
     }
@@ -166,6 +183,10 @@ class MainViewModel(
         }
     }
 
+    fun completeBotConsultation() {
+        chatListOfMessages = mutableStateListOf(START_MESSAGE)
+    }
+
     fun getNearestAppointment(): AppointmentsInLocalDateTime? {
         val notes = getAllBusinessAppointmentsInLDT().sortedBy { it.time }
         val nearestIndex = notes.indexOfFirst { it.time >= LocalDateTime.now() }
@@ -205,18 +226,25 @@ class MainViewModel(
         selectedDate = newDate
     }
 
+
     fun login(userRegisterInfoNetwork: UserRegisterInfoNetwork) {
-        viewModelScope.launch {
-            repository.login(userRegisterInfoNetwork)
-                .flowOn(Dispatchers.IO)
-                .catch { ex ->
-                    Timber.e(ex)
-                }
-                .collect {
-                    userId = it.id
-                }
-        }
+            viewModelScope.launch {
+                repository.login(userRegisterInfoNetwork)
+                    .flowOn(Dispatchers.IO)
+                    .catch { ex ->
+                        Timber.e(ex)
+                    }
+                    .collect {
+                        userId = it.id
+                        sharedPreferencesEditor.putString(USERID_TAG, userId)
+                    }
+            }
     }
+
+    fun loggedIn() = sharedPreferences.contains(USERID_TAG).also {
+        if (it) userId = sharedPreferences.getString(USERID_TAG, null)!!
+    }
+
 
     fun registerNewAccount(userRegisterInfoNetwork: UserRegisterInfoNetwork) {
         viewModelScope.launch {
@@ -227,6 +255,7 @@ class MainViewModel(
                 }
                 .collect {
                     userId = it.id
+                    sharedPreferencesEditor.putString(USERID_TAG, userId)
                 }
         }
     }
--- app/src/main/java/com/example/opencontrol/view/chatTab/ChatScreen.kt
+++ app/src/main/java/com/example/opencontrol/view/chatTab/ChatScreen.kt
@@ -1,21 +1,31 @@
 package com.example.opencontrol.view.chatTab
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.DismissDirection
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -30,16 +40,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.rotary.onPreRotaryScrollEvent
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.example.opencontrol.MainViewModel
 import com.example.opencontrol.model.ChatMessage
 import com.example.opencontrol.ui.theme.ChatGreenPrimary
 import com.example.opencontrol.ui.theme.ChatGreenSecondary
 import com.example.opencontrol.ui.theme.ChatGreyMessage
 import com.example.opencontrol.ui.theme.LightColors
+import com.example.opencontrol.ui.theme.md_theme_light_onPrimary
+import com.example.opencontrol.view.noteTab.NewNoteScreen
+import com.example.opencontrol.view.noteTab.NoteInfoScreen
+import com.example.opencontrol.view.noteTab.NoteScreen
+import okhttp3.internal.notify
 import org.koin.androidx.compose.getViewModel
+
 
 class ChatScreen : Screen {
     @Composable
@@ -56,7 +82,9 @@ private fun ChatScreenContent() {
 @Composable
 fun ChatWindow() {
     val viewModel = getViewModel<MainViewModel>()
-    val messages = viewModel.chatListOfMessages
+    val messages = remember {
+        viewModel.chatListOfMessages
+    }
 
     Column(
         modifier = Modifier
@@ -67,14 +95,24 @@ fun ChatWindow() {
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxSize()
-                .weight(8f)
+                .weight(4f)
         ) {
-            MessageList(messages = messages)
+            MessageList(messages = messages, modifier = Modifier.fillMaxSize())
         }
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .wrapContentHeight()
+        ) {
+            HelpButtons()
+            ConsultationCompleteButton()
+        }
+
         Box(
             modifier = Modifier
-                .fillMaxSize()
                 .weight(1f)
+                .wrapContentHeight()
         ) {
             SendMessageBar(
                 onSendMessage = { message ->
@@ -82,11 +120,12 @@ fun ChatWindow() {
                 }
             )
         }
+
     }
 }
 
 @Composable
-fun MessageList(messages: List<ChatMessage>) {
+fun MessageList(messages: List<ChatMessage>, modifier: Modifier) {
     val listState = rememberLazyListState()
 
     LaunchedEffect(key1 = messages.size) {
@@ -94,8 +133,7 @@ fun MessageList(messages: List<ChatMessage>) {
     }
 
     LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = modifier,
         verticalArrangement = Arrangement.Bottom,
         state = listState
     ) {
@@ -106,9 +144,92 @@ fun MessageList(messages: List<ChatMessage>) {
 }
 
 @Composable
+fun HelpButtons() {
+    val navigator = LocalNavigator.currentOrThrow
+    val viewModel = getViewModel<MainViewModel>()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        HelpButton(
+            text = "Записаться на консультацию",
+            onTap = { navigator.push(NewNoteScreen()) }
+        )
+        HelpButton(
+            text = "Ближайшая запись на консультацию",
+            onTap = {
+                viewModel.getNearestAppointment()
+                    ?.let {
+                        navigator.push(NoteInfoScreen(noteId = it.id))
+                    }
+            }
+        )
+        HelpButton(
+            text = "Оценить вероятность нарушений",
+            onTap = {}
+        )
+    }
+}
+
+@Composable
+fun HelpButton(
+    text: String,
+    onTap: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .clip(RoundedCornerShape(12))
+            .background(ChatGreenPrimary)
+            .clickable { onTap() }
+            .width(111.dp)
+            .height(80.dp)
+        //.fillMaxWidth(0.33f)
+        //.fillMaxHeight(0.2f)
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = md_theme_light_onPrimary,
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp,
+            modifier = Modifier.padding(horizontal = 8.dp),
+        )
+    }
+}
+
+@Composable
+fun ConsultationCompleteButton() {
+    val viewModel = getViewModel<MainViewModel>()
+    Box(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            //TODO: fix size
+            .clip(RoundedCornerShape(20))
+            .background(ChatGreenPrimary)
+            .clickable {
+                viewModel.completeBotConsultation()
+            }
+            .fillMaxWidth(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Text(
+            text = "Завершить консультацию",
+            color = md_theme_light_onPrimary,
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp,
+            modifier = Modifier.padding(horizontal = 56.dp, vertical = 14.dp),
+        )
+    }
+}
+
+@Composable
 fun MessageItem(message: ChatMessage) {
     Row(
-        modifier = Modifier.padding(vertical = 8.dp)
+        modifier = Modifier
+            .padding(vertical = 8.dp)
     ) {
         if (message.isSentByUser) {
             Spacer(modifier = Modifier.weight(1f))
@@ -118,20 +239,24 @@ fun MessageItem(message: ChatMessage) {
                 modifier = Modifier
                     .background(
                         color = ChatGreenPrimary,
-                        shape = RoundedCornerShape(8.dp, 8.dp, 0.dp, 8.dp)
+                        shape = RoundedCornerShape(27.dp, 27.dp, 2.dp, 27.dp)
                     )
-                    .padding(8.dp)
+                    .fillMaxWidth(0.8f)
+                    .padding(horizontal = 22.dp, vertical = 16.dp)
             )
         } else {
-            Text(
-                text = message.content,
-                modifier = Modifier
-                    .background(
-                        color = ChatGreyMessage,
-                        shape = RoundedCornerShape(8.dp, 8.dp, 8.dp, 0.dp)
-                    )
-                    .padding(8.dp)
-            )
+            Box {
+                Text(
+                    text = message.content,
+                    modifier = Modifier
+                        .background(
+                            color = ChatGreyMessage,
+                            shape = RoundedCornerShape(27.dp, 27.dp, 27.dp, 2.dp)
+                        )
+                        .fillMaxWidth(0.8f)
+                        .padding(horizontal = 22.dp, vertical = 16.dp)
+                )
+            }
             Spacer(modifier = Modifier.weight(1f))
         }
     }
@@ -146,7 +271,7 @@ fun SendMessageBar(onSendMessage: (ChatMessage) -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxSize()
-            .background(ChatGreenSecondary)
+            //.background(ChatGreenSecondary)
             .padding(vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
